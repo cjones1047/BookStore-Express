d2e6b6ea15166d01d06c9d915764fc226070acba
cjones1047 <cjmj1047@gmail.com>
2022-08-06 16:06:48 -0500

    Changed name of schema in like model from 'like' to 'likeSchema'

--- app/models/like.js
+++ app/models/like.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const like = new mongoose.Schema(
+const likeSchema = new mongoose.Schema(
     {
         owner:{
             type: mongoose.Schema.Types.ObjectId,
